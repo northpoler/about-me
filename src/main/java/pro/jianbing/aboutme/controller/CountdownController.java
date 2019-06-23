df20703153b5b67219d18b0ab00b593efe38f832
李建兵 <787331840@qq.com>
2019-06-23 17:09:28 +0800

    优化：倒计时的时间和标题从数据库读取

---
+++ src/main/java/pro/jianbing/aboutme/controller/CountdownController.java
@@ -0,0 +1,31 @@
+package pro.jianbing.aboutme.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.jianbing.aboutme.pojo.po.Countdown;
+import pro.jianbing.aboutme.service.CountdownService;
+import pro.jianbing.aboutme.service.LikeService;
+
+import java.util.List;
+
+@RequestMapping("countdown")
+@RestController
+public class CountdownController {
+
+    private final CountdownService countdownService;
+
+    @Autowired
+    public CountdownController(CountdownService countdownService) {
+        this.countdownService = countdownService;
+    }
+
+    @GetMapping("get")
+    public List<Countdown> getTwoCountdown(){
+        List<Countdown> twoCountdown = countdownService.getTwoCountdown();
+        return twoCountdown;
+    }
+}
--- src/main/java/pro/jianbing/aboutme/controller/IndexController.java
+++ src/main/java/pro/jianbing/aboutme/controller/IndexController.java
@@ -4,25 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import pro.jianbing.aboutme.pojo.po.User;
+import pro.jianbing.aboutme.service.CountdownService;
 import pro.jianbing.aboutme.service.LikeService;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class IndexController {
 
-    private final
-    LikeService likeService;
+    private final LikeService likeService;
 
     @Autowired
-    public IndexController(LikeService likeService) {
+    public IndexController(LikeService likeService, CountdownService countdownService) {
         this.likeService = likeService;
     }
 
     @GetMapping("/index")
-    public String Index(Model model){
+    public String index(Model model){
         int sumLikes = likeService.getSumLikes();
         model.addAttribute("likes",sumLikes);
         return "index";
