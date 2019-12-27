3a211ebe9c754e3d67fc8c8962e1e926de94b779
李建兵 <787331840@qq.com>
2019-12-27 13:30:34 +0800

    添加：天气工具，未完待续2

--- src/main/java/pro/jianbing/aboutme/util/WeatherUtil.java
+++ src/main/java/pro/jianbing/aboutme/util/WeatherUtil.java
@@ -1,14 +1,15 @@
 package pro.jianbing.aboutme.util;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import pro.jianbing.aboutme.pojo.WeatherDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,74 +24,37 @@ public final class WeatherUtil {
     private static final Logger logger = LoggerFactory.getLogger(WeatherUtil.class);
     private static final String UNKNOWN_LOCATION = "unknown location";
     private static final String LOCATION = "location";
+    /**
+     * 获取天气的接口：此处的location可以有以下几种类型：
+     * 1. 城市ID：城市列表
+     * 2. 经纬度格式：经度,纬度（经度在前纬度在后，英文,分隔，十进制格式，北纬东经为正，南纬西经为负
+     * 3. 城市名称，支持中英文和汉语拼音
+     * 4. 城市名称，上级城市 或 省 或 国家，英文,分隔，此方式可以在重名的情况下只获取想要的地区的天气数据，例如 西安,陕西
+     * 5. IP
+     * 6. 根据请求自动判断，根据用户的请求获取IP，通过 IP 定位并获取城市数据
+     * 示例数据如下：
+     * 1. location=CN101010100
+     * 2. location=116.40,39.9
+     * 3. location=北京、 location=北京市、 location=beijing
+     * 4. location=朝阳,北京、 location=chaoyang,beijing
+     * 5. location=60.194.130.1
+     * 6. location=auto_ip
+     */
+    private static final String URL = "https://free-api.heweather.net/s6/weather/now?key=3c3fa198cacc4152b94b20def11b2455@location=";
 
     /**
-     * 通过request请求获取天气
+     * 获取天气
      *
-     * @param request 请求
-     * @return
-     * @throws IOException 输入输出流异常
-     */
-    public static WeatherDto getWeather(HttpServletRequest request) {
-        // 通过请求，获取地址信息
-        String ipAddress = NetworkUtil.getIpAddress(request);
-        return getWeatherByIp(ipAddress);
-    }
-
-    /**
-     * 通过IP获取天气
-     *
-     * @param ip
      * @return
      */
-    public static WeatherDto getWeatherByIp(String ip) {
-        String cityName = NetworkUtil.getCityNameByIp(ip);
-        return getWeatherByCityName(cityName);
-    }
-
-    /**
-     * 通过城市名获取天气
-     * 接口：https://free-api.heweather.net/s6/weather/now?location=地区名称&key=3c3fa198cacc4152b94b20def11b2455
-     *
-     * @param cityName
-     * @return
-     */
-    public static WeatherDto getWeatherByCityName(String cityName) {
-        String url = "https://free-api.heweather.net/s6/weather/now?location="
-                + cityName +
-                "&key=3c3fa198cacc4152b94b20def11b2455";
+    public static WeatherDto getWeather() {
+        String url = URL + "auto_ip";
         String result = HttpUtil.doGet(url);
-        /*{"HeWeather6":[{"basic":{"cid":"CN101210101","location":"杭州","parent_city":"杭州","admin_area":"浙江",
-                "cnty":"中国","lat":"30.28745842","lon":"120.15357971","tz":"+8.00"},"update":
-            {"loc":"2019-12-26 20:15","utc":"2019-12-26 12:15"},"status":"ok","now":{"cloud":"91","" +
-                    "cond_code":"101","cond_txt":"多云","fl":"1","hum":"73","pcpn":"0.0","pres":"1023",
-                    "tmp":"6","vis":"16",
-                "wind_deg":"353","wind_dir":"北风","wind_sc":"3","wind_spd":"19"}}]}*/
-        /*{"HeWeather6":[{"basic":{"cid":"CN101010300","location":"朝阳","parent_city":"北京","admin_area":"北京",
-                "cnty":"中国","lat":"39.92148972","lon":"116.48641205","tz":"+8.00"},
-            "update":{"loc":"2019-12-26 20:00","utc":"2019-12-26 12:00"},"status":"ok",
-                    "now":{"cloud":"0","cond_code":"100","cond_txt":"晴","fl":"-3","hum":"32",
-                    "pcpn":"0.0","pres":"1021","tmp":"0","vis":"16","wind_deg":"257","wind_dir":
-                "西南风","wind_sc":"1","wind_spd":"4"}},{"basic":{"cid":"CN101071201","location":"朝阳",
-                "parent_city":"朝阳","admin_area":"辽宁","cnty":"中国","lat":"41.57675934","lon":"120.4511795",
-                "tz":"+8.00"},"update":{"loc":"2019-12-26 20:03","utc":"2019-12-26 12:03"},"status":"ok",
-                "now":{"cloud":"0","cond_code":"100","cond_txt":"晴","fl":"-13","hum":"61","pcpn":"0.0",
-                "pres":"1003","tmp":"-10","vis":"16","wind_deg":"280","wind_dir":"西风","wind_sc":"1",
-                "wind_spd":"3"}},{"basic":{"cid":"CN101060110","location":"朝阳","parent_city":"长春",
-                "admin_area":"吉林","cnty":"中国","lat":"43.86491013","lon":"125.31803894","tz":"+8.00"},
-            "update":{"loc":"2019-12-26 20:01","utc":"2019-12-26 12:01"},"status":"ok","now":{"cloud":"0",
-                    "cond_code":"101","cond_txt":"多云","fl":"-25","hum":"89","pcpn":"0.0","pres":"995",
-                    "tmp":"-21","vis":"10","wind_deg":"279","wind_dir":"西风","wind_sc":"1","wind_spd":"5"}}]}*/
-        /*未知地区示例数据：{"HeWeather6":[{"status":"unknown location"}]}*/
-        if (null == result || result.contains(UNKNOWN_LOCATION)) {
-            // 发送邮件
-
-            return null;
-        } else {
-            WeatherDto weatherDto = new WeatherDto();
-
-            return weatherDto;
+        WeatherDto dto = new WeatherDto();
+        if (!StringUtils.isEmpty(result) && !result.contains(UNKNOWN_LOCATION)){
+            Map resultMap = JSON.parseObject(result, Map.class);
         }
+        return dto;
     }
 
     /**
