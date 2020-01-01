4e01daf75499ce4ee07b7ec213bce276552a6404
李建兵 <787331840@qq.com>
2020-01-01 18:22:31 +0800

    优化

--- src/main/java/pro/jianbing/aboutme/common/dto/BaseResult.java
+++ src/main/java/pro/jianbing/aboutme/common/dto/BaseResult.java
@@ -10,7 +10,7 @@ public class BaseResult {
 
     private static final String SYSTEM_ERROR = "系统异常！";
 
-    public static BaseResult createBaseResult(boolean result, String message, Object data){
+    public static BaseResult createBaseResult(boolean result, String message, Object data) {
         BaseResult baseResult = new BaseResult();
         baseResult.setResult(result);
         baseResult.setMessage(message);
@@ -18,39 +18,39 @@ public class BaseResult {
         return baseResult;
     }
 
-    public static BaseResult success(String message, Object data){
-        return createBaseResult(true,message,data);
+    public static BaseResult success(String message, Object data) {
+        return createBaseResult(true, message, data);
     }
 
-    public static BaseResult success(String message){
-        return createBaseResult(true,message,null);
+    public static BaseResult success(String message) {
+        return createBaseResult(true, message, null);
     }
 
-    public static BaseResult success(Object data){
-        return createBaseResult(true,null,data);
+    public static BaseResult success(Object data) {
+        return createBaseResult(true, null, data);
     }
 
-    public static BaseResult success(){
-        return createBaseResult(true,null,null);
+    public static BaseResult success() {
+        return createBaseResult(true, null, null);
     }
 
-    public static BaseResult fail(String message, Object data){
-        return createBaseResult(false,message,data);
+    public static BaseResult fail(String message, Object data) {
+        return createBaseResult(false, message, data);
     }
 
-    public static BaseResult fail(String message){
-        return createBaseResult(false,message,null);
+    public static BaseResult fail(String message) {
+        return createBaseResult(false, message, null);
     }
 
-    public static BaseResult fail(Object data){
-        return createBaseResult(false,null,data);
+    public static BaseResult fail(Object data) {
+        return createBaseResult(false, null, data);
     }
 
-    public static BaseResult fail(){
-        return createBaseResult(false,null,null);
+    public static BaseResult fail() {
+        return createBaseResult(false, null, null);
     }
 
-    public static BaseResult systemError(){
-        return createBaseResult(false,SYSTEM_ERROR,null);
+    public static BaseResult systemError() {
+        return createBaseResult(false, SYSTEM_ERROR, null);
     }
 }
